2294c1f942defeb5febf598d2a8e9be9963dfdab
Wassim Daoudi Idrissi <wdaoudi-@made-f0Dr9s17.clusters.42paris.fr>
2025-01-10 18:11:10 +0100

    ajout

--- include/cub3d.h
+++ include/cub3d.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cub3d.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ayarab <ayarab@student.42.fr>              +#+  +:+       +#+        */
+/*   By: wdaoudi- <wdaoudi-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/09 16:03:47 by ayarab            #+#    #+#             */
-/*   Updated: 2025/01/09 21:08:52 by ayarab           ###   ########.fr       */
+/*   Updated: 2025/01/10 17:34:15 by wdaoudi-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,6 +31,33 @@ typedef struct s_cub3d
     char *NO;
     int    ac;
 }    t_cub3d;
+
+typedef struct s_player {
+    double posX;            // Position X du joueur
+    double posY;            // Position Y du joueur
+    double dirX;           // Vecteur de direction X
+    double dirY;           // Vecteur de direction Y
+    double planeX;         // Plan caméra X (pour le FOV)
+    double planeY;         // Plan caméra Y (pour le FOV)
+    double moveSpeed;      // Vitesse de déplacement
+    double rotSpeed;       // Vitesse de rotation
+} t_player;
+
+typedef struct s_ray {
+    double cameraX;        // Position X sur le plan caméra
+    double rayDirX;        // Direction X du rayon
+    double rayDirY;        // Direction Y du rayon
+    double sideDistX;      // Distance au prochain côté X
+    double sideDistY;      // Distance au prochain côté Y
+    double deltaDistX;     // Distance entre intersections X
+    double deltaDistY;     // Distance entre intersections Y
+    double perpWallDist;   // Distance perpendiculaire au mur
+    int mapX;             // Position actuelle dans la map (X)
+    int mapY;             // Position actuelle dans la map (Y)
+    int stepX;            // Direction du pas en X (-1 ou 1)
+    int stepY;            // Direction du pas en Y (-1 ou 1)
+    int side;             // Quel côté du mur est touché (NS ou EW)
+} t_ray;
 
 
 #endif
