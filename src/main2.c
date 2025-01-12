c68bcaf343cf7f181760c7f80d99397bae9ca42e
Wdaoudi <wassim.daoudi75@gmail.com>
2025-01-12 17:51:28 +0100

    tentative d ajout des textures

--- src/main2.c
+++ src/main2.c
@@ -6,7 +6,7 @@
 /*   By: wdaoudi- <wdaoudi-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/11 16:33:27 by wdaoudi-          #+#    #+#             */
-/*   Updated: 2025/01/12 17:08:51 by wdaoudi-         ###   ########.fr       */
+/*   Updated: 2025/01/12 17:33:06 by wdaoudi-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ int	main(int ac, char **av) // temporaire raycasting
 
 	mlx_hook(cube.win, 17, 0, close_window, &cube);
 	mlx_hook(cube.win, 2, 1L << 0, key_press, &cube);
-	mlx_loop_hook(cube.mlx, /* render frame qui dessine*/ NULL, &cube);
+	mlx_loop_hook(cube.mlx,NULL, &cube);
 	mlx_loop(cube.mlx);
 	cleanup(&cube);
 	return (0);
--- src/raycasting/init_data.c
+++ src/raycasting/init_data.c
@@ -6,7 +6,7 @@
 /*   By: wdaoudi- <wdaoudi-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/11 15:31:23 by wdaoudi-          #+#    #+#             */
-/*   Updated: 2025/01/12 17:28:44 by wdaoudi-         ###   ########.fr       */
+/*   Updated: 2025/01/12 17:32:25 by wdaoudi-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,46 +18,46 @@ int	init_cub3d(t_cub3d *cube)
 	ft_memset(cube, 0, sizeof(t_cub3d));
 	cube->screen_width = 1920;
 	cube->screen_height = 1080;
-	
-    if (init_mlx(cube) || !cube->mlx || !cube->win)
-		return (printf("fail to init MLX\n"),cleanup(cube),1);
-
+	if (init_mlx(cube) || !cube->mlx || !cube->win)
+		return (printf("fail to init MLX\n"), cleanup(cube), 1);
 	if (!init_textures(cube))
-		return (printf("fail to init textures\n"),cleanup(cube), 1);
-
+		return (printf("fail to init textures\n"), cleanup(cube), 1);
 	return (0);
 }
 
 int	init_textures(t_cub3d *cube)
 {
 	if (!load_texture(cube->mlx, &cube->textures.north,
-			"./textures/colorstone.xpm")) // a modifie plus tard avec le parsing
+						"./textures/colorstone.xpm"))
+							// a modifie plus tard avec le parsing
 		return (0);
 	if (!load_texture(cube->mlx, &cube->textures.south,
-			"./textures/bluestone.xpm")) // a modifie plus tard avec le parsing
+						"./textures/bluestone.xpm"))
+							// a modifie plus tard avec le parsing
 	{
 		mlx_destroy_image(cube->mlx, cube->textures.north.img);
 		mlx_destroy_image(cube->mlx, cube->textures.south.img);
 		return (0);
 	}
-        if (!load_texture(cube->mlx, &cube->textures.east, " ./textures/wood.xpm"))
-    {
-        mlx_destroy_image(cube->mlx, cube->textures.north.img);
-        mlx_destroy_image(cube->mlx, cube->textures.south.img);
-        mlx_destroy_image(cube->mlx, cube->textures.west.img);
-        return (0);
-    }
-    return (1);
+	if (!load_texture(cube->mlx, &cube->textures.east, " ./textures/wood.xpm"))
+	{
+		mlx_destroy_image(cube->mlx, cube->textures.north.img);
+		mlx_destroy_image(cube->mlx, cube->textures.south.img);
+		mlx_destroy_image(cube->mlx, cube->textures.west.img);
+		return (0);
+	}
+	printf(" init texture\n");
+	return (1);
 }
 
-int load_texture(void *mlx, t_img *img, char *path)
+int	load_texture(void *mlx, t_img *img, char *path)
 {
-    img->img= mlx_xpm_file_to_image(mlx,path, &img->width,&img->height);
-        if (!img->img)
-            return (0);
-    
-    img->addr = mlx_get_data_addr(img->img, &img->bits_per_pixel, &img->line_length, &img->endian);
-    if (!img->addr)
-        return (mlx_destroy_image(mlx,img->img),0);
-    return (1);
+	img->img = mlx_xpm_file_to_image(mlx, path, &img->width, &img->height);
+	if (!img->img)
+		return (0);
+	img->addr = mlx_get_data_addr(img->img, &img->bits_per_pixel,
+			&img->line_length, &img->endian);
+	if (!img->addr)
+		return (mlx_destroy_image(mlx, img->img), 0);
+	return (1);
 }
