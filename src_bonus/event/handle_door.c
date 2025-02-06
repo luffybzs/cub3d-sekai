9b6baf8f4701867cb1ec6071a718f38a94d382b6
Wdaoudi <wassim.daoudi75@gmail.com>
2025-02-06 17:17:43 +0100

    merge

--- src_bonus/event/handle_door.c
+++ src_bonus/event/handle_door.c
@@ -6,37 +6,37 @@
 /*   By: wdaoudi- <wdaoudi-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/06 14:23:39 by wdaoudi-          #+#    #+#             */
-/*   Updated: 2025/02/06 15:25:27 by wdaoudi-         ###   ########.fr       */
+/*   Updated: 2025/02/06 15:28:12 by wdaoudi-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/cub3d_bonus.h"
 
-int ft_check_door(t_cub3d *cube3d)
-{
-	int i = 0;
-	int j = 0;
-	bool door = false;
+// int ft_check_door(t_cub3d *cube3d)
+// {
+// 	int i = 0;
+// 	int j = 0;
+// 	bool door = false;
 
-	while (cube3d->all_maps[j])
-	{
-		i = 0;
-		while (cube3d->all_maps[j][i])
-		{
-			if ((cube3d->all_maps[j][i]) == 'D')
-			{
+// 	while (cube3d->all_maps[j])
+// 	{
+// 		i = 0;
+// 		while (cube3d->all_maps[j][i])
+// 		{
+// 			if ((cube3d->all_maps[j][i]) == 'D')
+// 			{
 				
-				cube3d->x_door = i;
-				cube3d->y_door = j;
-			}
-			i++;
-		}
-		j++;
-	}
-	if (player == false)
-		return (EXIT_FAILURE);
-	return (EXIT_SUCCESS);
-} 
+// 				cube3d->x_door = i;
+// 				cube3d->y_door = j;
+// 			}
+// 			i++;
+// 		}
+// 		j++;
+// 	}
+// 	if (player == false)
+// 		return (EXIT_FAILURE);
+// 	return (EXIT_SUCCESS);
+// } 
 
 void handle_door(t_cub3d *cube)
 {
